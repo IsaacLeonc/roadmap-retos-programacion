9bb26ea2e96cf27f4c7b161a1132effab7727315
IsaacLeonc <jisaacleonc@gmail.com>
2024-09-26 11:17:52 -0600

    Create IsaacLeonc.java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/IsaacLeonc.java
@@ -0,0 +1,30 @@
+ // #00 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO
+
+        // URL oficial de Java:https://www.java.com/es/
+        /*
+         * Comentario
+         * De
+         * Varias
+         * Lineas
+         */
+
+
+public class IsaacLeonc {
+    public static void main(String[] args) {
+       
+
+        String M_variable = "Variable";
+        final String M_costante = "Constante";
+
+        byte M_byte = 10;
+        short M_Short = 20;
+        int M_Int = 10000;
+        long M_Long = 10200;
+        char M_Char = 'a';
+        boolean M_Bool = false;
+
+        System.out.println("¡Hola, Java!");
+
+    }
+}
+
